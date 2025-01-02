2e73354406c5cc3b4d602ba1cf61529f54f6660c
webdevkaleem <webdevkaleem@gmail.com>
2025-01-02 23:11:07 +0500

    Preview Branch Testing

--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { HydrateClient } from "@/trpc/server";
 export default async function Home() {
   return (
     <HydrateClient>
-      <h1>Admin</h1>
+      <h1>Preview - Admin</h1>
     </HydrateClient>
   );
 }
