55fdc27e07f28d623f779e9b4ce303ab8d1498b6
webdevkaleem <webdevkaleem@gmail.com>
2025-02-05 20:27:42 +0500

    Cleaned

--- backend/src/app/products/page.tsx
+++ backend/src/app/products/page.tsx
@@ -1,26 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { api } from "@/trpc/server";
-import { Plus } from "lucide-react";
-import Link from "next/link";
-import DeleteAllModel from "@/components/admin/models/category/delete-all";
-import DeleteByIdModel from "@/components/admin/models/category/delete-by-id";
-import { labelToSlug, type SearchParamsType } from "@/lib/helper-functions";
-import { Suspense } from "react";
 import FadeIn from "@/components/admin/animations/fade-in";
 import Loader from "@/components/admin/animations/loader";
+import DeleteAllModel from "@/components/admin/models/category/delete-all";
+import DeleteByIdModel from "@/components/admin/models/category/delete-by-id";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { type CollectionsType } from "@/lib/const";
+import { labelToSlug } from "@/lib/helper-functions";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { Suspense } from "react";
 
 const collection: CollectionsType = {
   singular: "Product",
   plural: "Products",
 };
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: SearchParamsType;
-}) {
+export default function Page() {
   return (
     <>
       {/* Main Section */}
@@ -30,10 +25,6 @@ export default function Page({
         </FadeIn>
         <FadeIn>
           <Separator />
-        </FadeIn>
-
-        <FadeIn>
-          <SearchTable searchParams={searchParams} />
         </FadeIn>
 
         {/* Models */}
@@ -60,26 +51,4 @@ function HeroSection() {
       </Button>
     </div>
   );
-}
-
-async function SearchTable({
-  searchParams,
-}: {
-  searchParams: SearchParamsType;
-}) {
-  const {
-    page = "1",
-    pageSize = "10",
-    sortBy = "updatedAt",
-    sortOrder = "desc",
-  } = await searchParams;
-
-  const { data, maxPages, maxRows } = await api.category.all({
-    page,
-    pageSize,
-    sortBy,
-    sortOrder,
-  });
-
-  return <div className="flex flex-col gap-4"></div>;
 }
